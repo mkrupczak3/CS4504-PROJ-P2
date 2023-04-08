356c27b97d1ae2e2536253074697426bb46d0b81
Sahan Reddy <sahan.reddy.58@gmail.com>
2023-04-08 13:07:16 -0400

    Add build and run scripts

---
+++ Router/run.bash
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+if [[ ! -d "$SCRIPT_PATH/build" ]]; then
+    echo "Can't access build/ - did you run build.bash?" 1>&2
+    exit 1
+fi
+
+java -cp "$SCRIPT_PATH/build" Router "$@"
